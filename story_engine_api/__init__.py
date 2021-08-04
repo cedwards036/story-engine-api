c9aebd0f0475103f8ef5e4ddb3e8d11f6d3fb358
Christopher Edwards <cedwards036@gmail.com>
2021-08-03 20:39:27 -0400

    allow CORS

--- story_engine_api/__init__.py
+++ story_engine_api/__init__.py
@@ -4,6 +4,7 @@ import io
 import csv
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -13,6 +14,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     # Heroku's DATABASE_URL uses the outdated "postgres://" prefix which SQLAlchemy doesn't accept anymore
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
